2c3a5fe7512c4a0ae98824f2923f39d8ce581b08
Akanksha Acharya <akanksha4510471@gmail.com>
2021-05-06 14:38:57 +0530

    splitting if condition

--- tests/src/test/java/Api/CommonFunctions.java
+++ tests/src/test/java/Api/CommonFunctions.java
@@ -14,7 +14,7 @@ public class CommonFunctions {
     String userName = userserial;
     String password = apikey;
 
-    if(userName != null && !userName.isEmpty() && password != null && !password.isEmpty()) {
+    if (userName != null && !userName.isEmpty() && password != null && !password.isEmpty()) {
       apiClient = new FdsApiClient();
       apiClient.setConnectTimeout(30000);
       apiClient.setReadTimeout(30000);
@@ -32,11 +32,12 @@ public class CommonFunctions {
 
   public static void handleException(String method, ApiException e) throws ApiException {
     System.err.println("Exception when calling " + method);
-    if (e.getResponseHeaders() != null && e.getResponseHeaders().containsKey("x-factset-api-request-key") && 
-        e.getResponseHeaders().containsKey("x-datadirect-request-key")) {
+    if (e.getResponseHeaders() != null && e.getResponseHeaders().containsKey("x-factset-api-request-key")) {
       System.out.println("Request Id: " + e.getResponseHeaders().get("x-factset-api-request-key").get(0));
-      System.out.println("Data-Direct Key: " + e.getResponseHeaders().get("x-datadirect-request-key").get(0));
     }
+    if (e.getResponseHeaders() != null && e.getResponseHeaders().containsKey("x-datadirect-request-key")) {
+      System.out.println("Data-Direct Key: " + e.getResponseHeaders().get("x-datadirect-request-key").get(0));
+    }    
     System.out.println("Status code: " + e.getCode());
     System.out.println("Reason: " + e.getClientErrorResponse());
     e.printStackTrace();
