c74dc11425e16852011e797505c481eb0f271817
Kiranmai Sreeramoju <kiranmai.sreeramoju@factset.com>
2022-03-24 12:29:59 +0530

    added tests for FI and Quant Engines

--- examples/src/examples/QuantEngineSingleUnitFeatherExample.java
+++ examples/src/examples/QuantEngineSingleUnitFeatherExample.java
@@ -49,6 +49,7 @@ public class QuantEngineSingleUnitFeatherExample {
   private static String FQL_EXPR = "P_PRICE";
   private static String FQL_NAME = "Price (SCR)";
   private static String CALCULATION_UNIT_ID = "1";
+  private static Boolean OVERRIDE_UNIVERSAL_SCREEN_CALENDAR = false;
   
   public static void main(String[] args) throws InterruptedException, JsonProcessingException {
     try {
@@ -60,6 +61,7 @@ public class QuantEngineSingleUnitFeatherExample {
       fdsDate.setEndDate(QUANT_END_DATE);
       fdsDate.setFrequency(QUANT_FREQUENCY);
       fdsDate.setCalendar(QUANT_CALENDAR);
+      fdsDate.setOverrideUniversalScreenCalendar(OVERRIDE_UNIVERSAL_SCREEN_CALENDAR);
       fdsDate.setSource(QuantFdsDate.SourceEnum.FDSDATE);
       
       OneOfQuantDates dates = new OneOfQuantDates(fdsDate);
